5ea5759bb9f7a97be8f0f0255ab285640d818125
Skyler Calaman <54462713+Blckbrry-Pi@users.noreply.github.com>
2021-12-27 00:13:28 -0500

    Make barebones oauth system.

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import * as readline from "readline";
+
 /**
  * Returns a generator over a numeric range, similar to range() in Python.
  * @param a the start of the range (inclusive), or if b is not specified, the end of the range (exclusive)
@@ -13,3 +15,31 @@ export function* range(a: number, b?: number, step: number = 1): Generator<numbe
         for (let i = a; a <= b ? i < b : i > b; i+= step) yield i;
     }
 }
+
+export const addProperty = <T, X extends string, V>(obj: T, propName: X, value: V): T & Record<X, V> => {
+    // @ts-ignore
+    obj[propName] = value;
+    // @ts-ignore
+    return obj;
+};
+
+
+export const extendRlInterface = (rlInterface: readline.Interface) => {
+    return addProperty(
+        rlInterface,
+        "asyncQuestion",
+        function(this: readline.Interface, query: string): Promise<string> {
+            return new Promise(resolve => this.question(query, resolve));
+        },
+    );
+};
+
+export const askQuestion = async (question: string) => {
+    const rl = extendRlInterface(readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    }));
+    const response = await rl.asyncQuestion(question);
+    rl.close();
+    return response;
+};
